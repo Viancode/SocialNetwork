e835bc4bd0d1b37cdb29914c68202ba479915191
Viancode <anh.vv993@gmail.com>
2024-07-24 21:18:38 +0700

    fix: handle validation exception

--- src/main/java/com/example/socialnetwork/application/request/AuthRequest.java
+++ src/main/java/com/example/socialnetwork/application/request/AuthRequest.java
@@ -1,5 +1,8 @@
 package com.example.socialnetwork.application.request;
 
+import com.example.socialnetwork.common.ValidationRegex;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -8,10 +11,11 @@ import lombok.Value;
 @Value
 @Builder
 public class AuthRequest {
-
+    @Email(message = "Email should be valid", regexp = ValidationRegex.EMAIL_REGEX)
     @NotNull(message = "Email cannot be null")
     @NotBlank(message = "Email cannot be blank")
     String email;
+    @Pattern(message = "Password should be valid", regexp = ValidationRegex.PASSWORD_REGEX)
     @NotNull(message = "Password cannot be null")
     @NotBlank(message = "Password cannot be blank")
     String password;
