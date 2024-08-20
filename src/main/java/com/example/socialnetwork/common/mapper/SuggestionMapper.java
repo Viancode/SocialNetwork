8e1304b062df2bc6bbda66e161876c05a41fcd00
vietngo177 <viettngo177@gmail.com>
2024-08-20 15:35:07 +0700

    update friend response

--- src/main/java/com/example/socialnetwork/common/mapper/SuggestionMapper.java
+++ src/main/java/com/example/socialnetwork/common/mapper/SuggestionMapper.java
@@ -5,8 +5,6 @@ import com.example.socialnetwork.infrastructure.entity.Suggestion;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface SuggestionMapper {
     SuggestionMapper INSTANCE = Mappers.getMapper(SuggestionMapper.class);
